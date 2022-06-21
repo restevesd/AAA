a607d7c2408a3885f4808d39c6fcbdddd8ddeb56
Roberto Esteves <info@robertoesteves.com>
2022-06-21 14:21:50 -0500

    Add files via upload

--- AAACasados.py
+++ AAACasados.py
@@ -1,14 +1,9 @@
 import streamlit as st
 import pandas as pd
-import re
-from datetime import date
-from datetime import datetime
 import streamlit.components.v1 as components
 from pycaret.classification import *
+import numpy as np
 
-import sklearn
-
-print('The scikit-learn version is {}.'.format(sklearn.__version__))
 
 def app():
     st.title("AAA Casados")
@@ -18,7 +13,7 @@ def app():
     estado_civil = st.selectbox("Estado Civil",("Casado","Unión Libre"))
     cargas = st.text_input("Número de cargas")
     vivienda = st.selectbox("Tipo de Vivienda",("Propia","Arrendada","Hipotecada","Prestada","Vive con familiares"))
-    domicilio = st.text_input("Domicilio")
+    domicilio = st.text_input("Tiempo Domicilio")
     educacion = st.selectbox("Nivel de Educación",("Secundaria","Técnica","Superior","Postgrado","Especialización","Doctorado"))
     antiguedad = st.text_input("Antigüedad Laboral")
     edad_cyg = st.text_input("Edad Cónyugue")
@@ -28,7 +23,8 @@ def app():
     total_egresos = st.text_input("Total Egresos")
     total_activos = st.text_input("Total Activos")
     total_pasivos = st.text_input("Total Pasivos")
-    total_patrimonio = st.text_input("Total Patrimonio")
+    total_patrimonio =st.text_input("Total Patrimonio")
+    #st.write("Total Patrimonio",float(total_activos)-float(total_pasivos))
     tipo_credito = st.selectbox("Tipo de Crédito",("Consumo","Vivienda"))
     tipo_garantia = st.selectbox("Tipo de Garantía",("HIP","PRE","QUI","Sin Garantía"))
     relacion_laboral = st.selectbox("Relación Laboral",("Dependiente","Independiente"))
@@ -37,32 +33,27 @@ def app():
     score_cyg = st.text_input("Score Conyugue")
     cuota_estimada = st.text_input("Couta estimada")
     cuota_sistema_financiero = st.text_input("Couta Sistema Financiero")
-    resultado = st.text_input("Resultado")
  
     if st.button("Analizar"):
         st.info('analizando')
-        datos_nuevos = [genero,edad,estado_civil,cargas,vivienda,
-                        domicilio,educacion,antiguedad,edad_cyg,educacion_cyg,
-                        antiguedad_lab_cyg,total_ingresos,total_egresos,total_activos,
-                        total_pasivos,total_patrimonio,tipo_credito,tipo_garantia,
-                        relacion_laboral,score,relacion_laboral_cyg,score_cyg,cuota_estimada,
-                        cuota_sistema_financiero] 
-        
-        columns={"genero":genero,"edad":edad,"estado_civil":estado_civil,
-                 "cargas":cargas,"vivienda":vivienda,"domicilio":domicilio,"educacion":educacion,
-                 "antiguedad":antiguedad,"edad_cyg":edad_cyg,"educacion_cyg":educacion_cyg,
-                 "antiguedad_lab":antiguedad_lab_cyg,"total_ingresos":total_ingresos,
-                 "total_egresos":total_egresos,"total_activos":total_activos,
-                 "total_pasivos":total_pasivos,"total_patrimonio":total_patrimonio,
+
+
+        columns={"genero":genero,"edad":int(edad),"estado_civil":estado_civil,
+                 "cargas":int(cargas),"vivienda":vivienda,"domicilio":domicilio,"educacion":educacion,
+                 "antiguedad":antiguedad,"edad_cyg":int(edad_cyg),"educacion_cyg":educacion_cyg,
+                 "antiguedad_lab":int(antiguedad_lab_cyg),"total_ingresos":float(total_ingresos),
+                 "total_egresos":float(total_egresos),"total_activos":float(total_activos),
+                 "total_pasivos":float(total_pasivos),"total_patrimonio":float(total_patrimonio),
                  "tipo_credito":tipo_credito,"tipo_garantia":tipo_garantia,
-                 "relacion_laboral":relacion_laboral,"score":score,
-                 "relacion_laboral_cyg":relacion_laboral_cyg,"score_cyg":score_cyg,
-                 "cuota_estimada":cuota_estimada,"cuota_sistema_financiero":cuota_sistema_financiero}
+                 "relacion_laboral":relacion_laboral,"score":float(score),
+                 "relacion_laboral_cyg":relacion_laboral_cyg,"score_cyg":float(score_cyg),
+                 "cuota_estimada":float(cuota_estimada),"cuota_sistema_financiero":float(cuota_sistema_financiero)}
         data = pd.DataFrame([columns])
-        print(data)
+        print(data.info())
         model = load_model("LGBM")
-        resultado = model.predict(data)   
-        print(resultado)
-        
+        label = ["AAA","REVISION"]
+        resultado =np.argmax(model.predict_proba(data))   
+        st.success("El resultado es: "+label[resultado])
+        print("Resultado: ",label[resultado])
 if __name__ == "__main__":
 	app()
